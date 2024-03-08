86c0ebb720c67479f893505d50c44088b1ee1c09
Yunsu0407 <aszx4949@naver.com>
2024-03-08 16:12:56 +0900

    한윤수 2021113405

--- Student.java
+++ Student.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 public class Student {
 	public static void act(Scanner oInDev) {
 		String sName;
-		//String sMajor;
+		String sMajor;
 		int iStuNo;
 		
 		System.out.print("이름: ");
 		sName = oInDev.next();
 		System.out.print("학번: ");
 		iStuNo = oInDev.nextInt();
-		//System.out.print("학과: ");
-		//sMajor = oInDev.next();
+		System.out.print("학과: ");
+		sMajor = oInDev.next();
 		
 		
 		System.out.println("이름: " + sName);
 		System.out.println("학번: " + iStuNo);
-		//System.out.println("학과: " + sMajor);
+		System.out.println("학과: " + sMajor);
 	}
 	
 	public static void main(String[] args) {
